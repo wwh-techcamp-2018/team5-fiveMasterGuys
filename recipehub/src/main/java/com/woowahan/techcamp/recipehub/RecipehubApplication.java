165b15708bd7c109b0819aeadfff70cc851ff5c1
Nooheat <nooheat1228@gmail.com>
2018-08-13 17:18:59 +0900

    Initialize Project

---
+++ recipehub/src/main/java/com/woowahan/techcamp/recipehub/RecipehubApplication.java
@@ -0,0 +1,12 @@
+package com.woowahan.techcamp.recipehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipehubApplication.class, args);
+	}
+}
---
+++ recipehub/src/test/java/com/woowahan/techcamp/recipehub/RecipehubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woowahan.techcamp.recipehub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipehubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
