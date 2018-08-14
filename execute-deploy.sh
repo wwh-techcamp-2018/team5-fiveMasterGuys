87e6282f9a55e5be477606bac8635a4c03f30471
Nooheat <nooheat1228@gmail.com>
2018-08-14 15:36:06 +0900

    chore: Apply Travis CI working with AWS CodeDeploy
    - Add deploy policy

---
+++ execute-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/travis
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f recipehub)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/build/build/libs/*.jar $REPOSITORY/jar/
+
+JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'recipehub' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/jar/$JAR_NAME &
