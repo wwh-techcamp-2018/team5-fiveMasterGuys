f4f308239510e0ef21948a0cb2834486f5ec491f
Ryunhee Han <ryuneeee@gmail.com>
2018-08-16 04:15:56 +0900

    feat: Create RecipeRestController (doing)
    - First Acceptance Test
    - Using MockMvcTest

--- src/test/java/com/woowahan/techcamp/recipehub/support/AcceptanceTest.java
+++ src/test/java/com/woowahan/techcamp/recipehub/support/AcceptanceTest.java
@@ -1,15 +1,31 @@
 package com.woowahan.techcamp.recipehub.support;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class AcceptanceTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    protected MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
 
     @Test
     public void name() throws Exception {
