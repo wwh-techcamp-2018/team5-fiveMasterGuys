e676191798bc4784e11da6c6b58ffcd331fa99ab
minzzzy <minpjh@gmail.com>
2018-08-16 20:26:50 +0900

    refactor: Add response status in conflictException

--- src/main/java/com/woowahan/techcamp/recipehub/exception/ConflictException.java
+++ src/main/java/com/woowahan/techcamp/recipehub/exception/ConflictException.java
@@ -1,4 +1,8 @@
 package com.woowahan.techcamp.recipehub.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ConflictException extends RuntimeException {
 }
