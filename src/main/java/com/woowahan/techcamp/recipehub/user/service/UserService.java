95aedebc6eade381606cf5355719b253a5f3ca00
minzzzy <minpjh@gmail.com>
2018-08-16 21:39:20 +0900

    fix: Fix wrong package path

--- src/main/java/com/woowahan/techcamp/recipehub/user/service/UserService.java
+++ src/main/java/com/woowahan/techcamp/recipehub/user/service/UserService.java
@@ -1,10 +1,8 @@
 package com.woowahan.techcamp.recipehub.user.service;
 
-import com.woowahan.techcamp.recipehub.common.exception.UnauthorizedException;
 import com.woowahan.techcamp.recipehub.exception.ConflictException;
 import com.woowahan.techcamp.recipehub.user.domain.User;
 import com.woowahan.techcamp.recipehub.user.domain.UserRepository;
-import com.woowahan.techcamp.recipehub.user.dto.LoginDto;
 import com.woowahan.techcamp.recipehub.user.dto.SignupDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,18 +10,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-    public User login(LoginDto dto) {
-        return userRepository.findByEmail(dto.getEmail())
-                .filter(user -> user.matchPassword(dto.getPassword(), passwordEncoder))
-                .orElseThrow(UnauthorizedException::new);
-    }
 
     public User add(SignupDto dto) {
         if (userRepository.existsByEmail(dto.getEmail())) {
