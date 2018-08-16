f76d4e4f0041885dc4fe15bccd8f3c24e0d0d677
ffaass <rlatnghd95@gmail.com>
2018-08-16 16:17:30 +0900

    refactor: Optimize import

--- src/test/java/com/woowahan/techcamp/recipehub/recipe/controller/RecipeAcceptanceTest.java
+++ src/test/java/com/woowahan/techcamp/recipehub/recipe/controller/RecipeAcceptanceTest.java
@@ -2,11 +2,7 @@ package com.woowahan.techcamp.recipehub.recipe.controller;
 
 import com.woowahan.techcamp.recipehub.support.AcceptanceTest;
 import org.junit.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.MvcResult;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
