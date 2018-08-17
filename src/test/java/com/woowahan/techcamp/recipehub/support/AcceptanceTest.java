488c055b74f171d1041fc5a2bec0e03c32d51939
Ryunhee Han <ryuneeee@gmail.com>
2018-08-17 21:34:49 +0900

    feat: Add RestResponse to support
    Resolves: #57

--- src/test/java/com/woowahan/techcamp/recipehub/support/AcceptanceTest.java
+++ src/test/java/com/woowahan/techcamp/recipehub/support/AcceptanceTest.java
@@ -2,6 +2,7 @@ package com.woowahan.techcamp.recipehub.support;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woowahan.techcamp.recipehub.common.support.RestResponse;
 import com.woowahan.techcamp.recipehub.user.domain.User;
 import com.woowahan.techcamp.recipehub.user.domain.UserRepository;
 import org.junit.After;
@@ -9,6 +10,9 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,6 +21,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,6 +30,9 @@ public abstract class AcceptanceTest {
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private TestRestTemplate template;
 
     @Autowired
     private UserRepository userRepository;
@@ -47,6 +55,40 @@ public abstract class AcceptanceTest {
     @After
     public void tearDown() throws Exception {
         userRepository.deleteAll();
+    }
+
+    public TestRestTemplate template(User user) {
+        return (user == null) ? template : template.withBasicAuth(user.getEmail(), user.getPassword());
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+
+    protected <T, R> ResponseEntity<RestResponse<R>> request(String path, HttpMethod method, T dto, User user, ParameterizedTypeReference<RestResponse<R>> typeRef) {
+        return template(user).exchange(path, method, new HttpEntity<>(dto, getHeaders()), typeRef);
+    }
+
+    protected <T, R> ResponseEntity<RestResponse<R>> request(String path, HttpMethod method, T dto, ParameterizedTypeReference<RestResponse<R>> typeRef) {
+        return request(path, method, dto, null, typeRef);
+    }
+
+    protected <T, R> ResponseEntity<RestResponse<R>> request(String path, HttpMethod method, ParameterizedTypeReference<RestResponse<R>> typeRef) {
+        return request(path, method, null, null, typeRef);
+    }
+
+    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestList(String path, HttpMethod method, T dto, User user, ParameterizedTypeReference<RestResponse<List<R>>> typeref) {
+        return template(user).exchange(path, method, new HttpEntity<>(dto, getHeaders()), typeref);
+    }
+
+    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestList(String path, HttpMethod method, T dto, ParameterizedTypeReference<RestResponse<List<R>>> typeRef) {
+        return requestList(path, method, dto, null, typeRef);
+    }
+
+    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestList(String path, HttpMethod method, ParameterizedTypeReference<RestResponse<List<R>>> typeRef) {
+        return requestList(path, method, null, null, typeRef);
     }
 
     protected static byte[] convertObjectToJsonBytes(Object object) throws IOException {
