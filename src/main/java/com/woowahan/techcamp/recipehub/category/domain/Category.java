736f12cbd38ad63514a4ff22b72303b98a31fa1d
Ryunhee Han <ryuneeee@gmail.com>
2018-08-19 08:22:46 +0900

    feat: Add CategoryRepository
    - Add CategoryRepository
    - Refactor Category for persist

--- src/main/java/com/woowahan/techcamp/recipehub/category/domain/Category.java
+++ src/main/java/com/woowahan/techcamp/recipehub/category/domain/Category.java
@@ -1,14 +1,25 @@
 package com.woowahan.techcamp.recipehub.category.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Builder
+    public Category(String title) {
+        this.title = title;
+    }
 }
