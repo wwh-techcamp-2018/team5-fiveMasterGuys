5c6f02a463d42a07010f9347ff6e9d60eefb577f
Ryunhee Han <ryuneeee@gmail.com>
2018-08-20 14:04:23 +0900

    feat: Add request client for Web AcceptanceTest (#69)
    * feat: Add request client for Web AcceptanceTest
    
    - Add dependency org.apache.httpcomponents:httpclient (test scope)
    Refer: https://github.com/spring-projects/spring-security-oauth/issues/441#issuecomment-345188476
    
    See Also: #47
    
    * feat: Add API/Web errors handling
    
    - Add handling validation exceptions (Web/JSON)
    - Add handling NotFound/BadRequest exceptions
    - Add NotFound Page / Redirection

---
+++ src/main/java/com/woowahan/techcamp/recipehub/common/support/WebControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.woowahan.techcamp.recipehub.common.support;
+
+import com.woowahan.techcamp.recipehub.common.exception.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class WebControllerAdvice {
+    private static final Logger log = LoggerFactory.getLogger(WebControllerAdvice.class);
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ModelAndView handleError(BindException exception) {
+        return new ModelAndView("redirect:/");
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ModelAndView handleBadRequest(BadRequestException exception) {
+        return new ModelAndView("redirect:/");
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(EntityNotFoundException exception) {
+        return "/notfound";
+    }
+}
--- src/test/java/com/woowahan/techcamp/recipehub/support/AcceptanceTest.java
+++ src/test/java/com/woowahan/techcamp/recipehub/support/AcceptanceTest.java
@@ -18,10 +18,14 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -40,13 +44,19 @@ public abstract class AcceptanceTest {
     protected MockMvc mvc;
     protected final String DEFAULT_USER_EMAIL = "WeAreTheBestTeam@recipehub.com";
     protected final String DEFAULT_USER_PASSWORD = "password1234!";
+    protected User basicAuthUser;
+    protected User savedUser;
 
     @Before
     public void setUp() throws Exception {
-        userRepository.save(User.builder()
+        User.UserBuilder userBuilder = User.builder()
                 .email(DEFAULT_USER_EMAIL)
                 .password(new BCryptPasswordEncoder().encode(DEFAULT_USER_PASSWORD))
-                .name("Team5").build());
+                .name("Team5");
+        savedUser = userRepository.save(userBuilder.build());
+
+        basicAuthUser = userBuilder.password(DEFAULT_USER_PASSWORD).build();
+
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .build();
@@ -61,34 +71,90 @@ public abstract class AcceptanceTest {
         return (user == null) ? template : template.withBasicAuth(user.getEmail(), user.getPassword());
     }
 
-    private HttpHeaders getHeaders() {
+    private HttpHeaders getJsonHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return headers;
     }
 
-    protected <T, R> ResponseEntity<RestResponse<R>> request(String path, HttpMethod method, T dto, User user, ParameterizedTypeReference<RestResponse<R>> typeRef) {
-        return template(user).exchange(path, method, new HttpEntity<>(dto, getHeaders()), typeRef);
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        return headers;
     }
 
-    protected <T, R> ResponseEntity<RestResponse<R>> request(String path, HttpMethod method, T dto, ParameterizedTypeReference<RestResponse<R>> typeRef) {
-        return request(path, method, dto, null, typeRef);
+    protected <T, R> ResponseEntity<RestResponse<R>> requestJson(String path, HttpMethod method, T dto, User user, ParameterizedTypeReference<RestResponse<R>> typeRef) {
+        return template(user).exchange(path, method, new HttpEntity<>(dto, getJsonHeaders()), typeRef);
     }
 
-    protected <T, R> ResponseEntity<RestResponse<R>> request(String path, HttpMethod method, ParameterizedTypeReference<RestResponse<R>> typeRef) {
-        return request(path, method, null, null, typeRef);
+    protected <T, R> ResponseEntity<RestResponse<R>> requestJson(String path, HttpMethod method, T dto, ParameterizedTypeReference<RestResponse<R>> typeRef) {
+        return requestJson(path, method, dto, null, typeRef);
     }
 
-    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestList(String path, HttpMethod method, T dto, User user, ParameterizedTypeReference<RestResponse<List<R>>> typeRef) {
-        return template(user).exchange(path, method, new HttpEntity<>(dto, getHeaders()), typeRef);
+    protected <R> ResponseEntity<RestResponse<R>> requestJson(String path, HttpMethod method, ParameterizedTypeReference<RestResponse<R>> typeRef) {
+        return requestJson(path, method, null, null, typeRef);
     }
 
-    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestList(String path, HttpMethod method, T dto, ParameterizedTypeReference<RestResponse<List<R>>> typeRef) {
-        return requestList(path, method, dto, null, typeRef);
+    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestJsonList(String path, HttpMethod method, T dto, User user, ParameterizedTypeReference<RestResponse<List<R>>> typeref) {
+        return template(user).exchange(path, method, new HttpEntity<>(dto, getJsonHeaders()), typeref);
     }
 
-    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestList(String path, HttpMethod method, ParameterizedTypeReference<RestResponse<List<R>>> typeRef) {
-        return requestList(path, method, null, null, typeRef);
+    protected <T, R> ResponseEntity<RestResponse<List<R>>> requestJsonList(String path, HttpMethod method, T dto, ParameterizedTypeReference<RestResponse<List<R>>> typeRef) {
+        return requestJsonList(path, method, dto, null, typeRef);
+    }
+
+    protected <R> ResponseEntity<RestResponse<List<R>>> requestJsonList(String path, HttpMethod method, ParameterizedTypeReference<RestResponse<List<R>>> typeRef) {
+        return requestJsonList(path, method, null, null, typeRef);
+    }
+
+    protected ResponseEntity<String> requestGet(String path, User user) {
+        return template(user).getForEntity(path, String.class);
+    }
+
+    protected ResponseEntity<String> requestGet(String path) {
+        return template(null).getForEntity(path, String.class);
+    }
+
+    protected <T> ResponseEntity<String> requestPost(String path, T dto, User user) {
+        return template(user).postForEntity(path, request(dto, null), String.class);
+    }
+
+    protected <T> ResponseEntity<String> requestPost(String path, T dto) {
+        return requestPost(path, dto, null);
+    }
+
+    protected <T> ResponseEntity<String> requestPut(String path, T dto, User user) {
+        return template(user).postForEntity(path, request(dto, "put"), String.class);
+    }
+
+    protected <T> ResponseEntity<String> requestPut(String path, T dto) {
+        return requestPut(path, dto, null);
+    }
+
+    protected ResponseEntity<String> requestDelete(String path, User user) {
+        return template(user).postForEntity(path, request(null, "delete"), String.class);
+    }
+
+    protected ResponseEntity<String> requestDelete(String path) {
+        return requestDelete(path, null);
+    }
+
+    private <T> HttpEntity<MultiValueMap<String, Object>> request(T dto, String method) {
+        MultiValueMap<String, Object> params = getBodyParams(dto);
+        if (method != null) params.add("_method", method);
+        return new HttpEntity<>(params, getHeaders());
+    }
+
+    private MultiValueMap<String, Object> getBodyParams(Object dto) {
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        if(dto == null) return params;
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = mapper.convertValue(dto, Map.class);
+        map.entrySet().stream()
+                .filter(e -> e.getValue() != null)
+                .forEach(e -> params.add(e.getKey(), String.valueOf(e.getValue())));
+        return params;
     }
 
     protected static byte[] convertObjectToJsonBytes(Object object) throws IOException {
