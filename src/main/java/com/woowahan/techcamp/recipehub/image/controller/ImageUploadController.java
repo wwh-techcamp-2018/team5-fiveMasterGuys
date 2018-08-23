f8328cb3730bb8d675ae66572c433c60e526d730
ffaass <rlatnghd95@gmail.com>
2018-08-23 18:19:14 +0900

    refactor: Change RestController annotation on ImageUploadController
    - Change @RestController to @Controller + @ResponseBody

--- src/main/java/com/woowahan/techcamp/recipehub/image/controller/ImageUploadController.java
+++ src/main/java/com/woowahan/techcamp/recipehub/image/controller/ImageUploadController.java
@@ -6,13 +6,14 @@ import com.woowahan.techcamp.recipehub.image.exception.InvalidFileException;
 import com.woowahan.techcamp.recipehub.image.service.ImageStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 
-@RestController
+@Controller
 @RequestMapping("/images")
 public class ImageUploadController {
 
@@ -22,6 +23,7 @@ public class ImageUploadController {
     @PostMapping
     @AuthRequired
     @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
     public RestResponse<String> handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException, InvalidFileException {
         return RestResponse.success(imageStorageService.store(file));
     }
@@ -29,12 +31,14 @@ public class ImageUploadController {
 
     @ExceptionHandler(IOException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
     public RestResponse<?> handleIOException(IOException exception) {
         return RestResponse.error("파일 저장 실패").build();
     }
 
     @ExceptionHandler(InvalidFileException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
     public RestResponse<?> handleInvalidFileException(InvalidFileException exception) {
         return RestResponse.error("").build();
     }
