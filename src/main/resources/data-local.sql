05d686ed856dc9834e7b71231a563d3023cb8f63
Chanmyung Yun <nooheat1228@gmail.com>
2018-08-24 13:10:39 +0900

    feat: Set recipe default image (#104)
    * feat: Set recipe default image
    
    - Add recipe default image 'recipe-default.png'
    - Set imgUrl to "/img/recipe-default.png" when parameter imgUrl is null
      in the Recipe's constructor
    
    * feat: Make Recipe class to contains default image url
    
    - Make the recipe class to contains default image url
    - Use Recipe.DEFAULT_IMAGE_URL in the test cases
    - Add test case for recipe's constructor call by image url is null
    
    Resolves: #98

--- src/main/resources/data-local.sql
+++ src/main/resources/data-local.sql
@@ -7,7 +7,7 @@ INSERT INTO user (email, password, name) VALUES ('test2@er.com', '$2a$10$6dbJkT9
 INSERT INTO category(title) VALUES('a');
 
 -- Completed recipe
-INSERT INTO recipe (owner_id, name, completed, img_url, created_at, updated_at) VALUES (1, '로즈마리 후추 버거', true, 'https://cdn.bmf.kr/_data/product/I21A3/a5ae10184ec276667e0a35e6f3012f20.jpg', now(), now());
+INSERT INTO recipe (owner_id, name, completed, img_url, created_at, updated_at) VALUES (1, '로즈마리 후추 버거', true, '/img/recipe-default.png', now(), now());
 INSERT INTO step (type, name, content, sequence, closed, rejected, recipe_id, writer_id, img_url) VALUES ('Step', '버거 모양 만들기1', '["빵을 얹는다", "고기를 올린다"]', 1, false, false, 1, 1, 'https://cdn.bmf.kr/_data/product/IF088/2dee3415155804a5e9dc7c24fd21d96c.jpg');
 
 INSERT INTO step (type, name, content, sequence, closed, rejected, recipe_id, writer_id, img_url) VALUES ('Step', '버거 모양 만들기2', '["소스를 뿌린다"]', 2, false, false, 1, 2, 'https://cdn.bmf.kr/_data/product/IF088/2dee3415155804a5e9dc7c24fd21d96c.jpg');
@@ -16,7 +16,7 @@ INSERT INTO step (type, name, content, sequence, closed, rejected, recipe_id, wr
 INSERT INTO step (type, name, content, sequence, closed, rejected, recipe_id, writer_id, img_url) VALUES ('Step', '이건 안보여야됨', '["빵을 얹는다", "고기를 올린다"]', 4, true, false, 1, 3, 'https://cdn.bmf.kr/_data/product/IF088/2dee3415155804a5e9dc7c24fd21d96c.jpg');
 
 -- Incompleted recipe
-INSERT INTO recipe (owner_id, name, completed, img_url, created_at, updated_at) VALUES (1, '김치 피자', false, 'https://cdn.bmf.kr/_data/product/I21A3/a5ae10184ec276667e0a35e6f3012f20.jpg', now(), now());
+INSERT INTO recipe (owner_id, name, completed, img_url, created_at, updated_at) VALUES (1, '김치 피자', false, '/img/recipe-default.png', now(), now());
 
 INSERT INTO step (type, name, content, sequence, closed, rejected, recipe_id, writer_id, img_url) VALUES ('Step', '피자 모양 만들기1', '["빵을 얹는다", "고기를 올린다"]', 1, false, false, 2, 1, 'https://cdn.bmf.kr/_data/product/IF088/2dee3415155804a5e9dc7c24fd21d96c.jpg');
 INSERT INTO step (type, name, content, sequence, closed, rejected, recipe_id, writer_id, img_url) VALUES ('Step', '피자 모양 만들기2', '["치즈를 올린다", "고기를 올린다"]', 2, false, false, 2, 2, 'https://cdn.bmf.kr/_data/product/IF088/2dee3415155804a5e9dc7c24fd21d96c.jpg');
