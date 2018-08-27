a2c4cb7c9434ba3b9831b29d582568753921adeb
Nooheat <nooheat1228@gmail.com>
2018-08-27 18:01:10 +0900

    fix: Make plus button doesn't append value when the value is empty
    Resolves: #133

--- src/main/resources/static/js/steps/step.js
+++ src/main/resources/static/js/steps/step.js
@@ -56,11 +56,7 @@ class StepManager {
 
     handleKeyUpEvent(e) {
         if (e.target.classList.contains('step-item-input') && e.keyCode === 13) {
-            if (e.target.value !== "") {
-                let stepId = e.target.closest('.box').getAttribute('data-step-id');
-                this.appendStepItem(e.target);
-                $(`.box[data-step-id="${stepId}"] .step-item-input`).focus();
-            }
+            this.appendStepItem(e.target);
             return;
         }
     }
@@ -106,12 +102,17 @@ class StepManager {
     }
 
     appendStepItem(target) {
+        const stepId = target.closest('.box').getAttribute('data-step-id');
         const stepItem = target.closest('.step-item');
         const value = stepItem.querySelector('input').value;
+        if (value.trim() === '') {
+            return;
+        }
         const stepContainer = target.closest('ol.step-contents');
         removeElement(stepItem);
         stepContainer.insertAdjacentHTML('beforeend', Templates.templateStepContentListItem(value));
         stepContainer.insertAdjacentHTML('beforeend', Templates.templateStepContentInput());
+        $(`.box[data-step-id="${stepId}"] .step-item-input`).focus();
     }
 
     removeStepItem(target) {
