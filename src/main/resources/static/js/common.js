274d5257d8fe0305467eb7367f19bcf05d50a28c
Lahavas <uv187sn@naver.com>
2018-08-29 13:03:11 +0900

    chore: remove toggleButton method in common.js

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -86,13 +86,3 @@ function autoGrow(element) {
     element.style.height = "5px";
     element.style.height = (element.scrollHeight)+"px";
 }
-
-function toggleButton(target) {
-    if (target.disabled) {
-        target.disabled = false;
-        return;
-    }
-
-    target.disabled = true;
-    return;
-}
