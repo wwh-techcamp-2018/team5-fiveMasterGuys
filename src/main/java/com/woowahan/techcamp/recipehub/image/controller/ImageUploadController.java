7e51ada8ab6b0a6c56161bbcc6e53f851e53fc02
Lahavas <uv187sn@naver.com>
2018-08-30 13:43:14 +0900

    feat: Apply exception message when image size is exceeded

--- src/main/java/com/woowahan/techcamp/recipehub/image/controller/ImageUploadController.java
+++ src/main/java/com/woowahan/techcamp/recipehub/image/controller/ImageUploadController.java
@@ -1,33 +1,43 @@
 package com.woowahan.techcamp.recipehub.image.controller;
 
 import com.woowahan.techcamp.recipehub.common.security.AuthRequired;
+import com.woowahan.techcamp.recipehub.common.support.Message;
 import com.woowahan.techcamp.recipehub.common.support.RestResponse;
 import com.woowahan.techcamp.recipehub.image.exception.InvalidFileException;
 import com.woowahan.techcamp.recipehub.image.service.ImageStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 
 
-@Controller
+@RestController
 @RequestMapping("/images")
 public class ImageUploadController {
 
+    private final int imageMaxSize = 5 * 1024 * 1024;
+
     @Autowired
     private ImageStorageService imageStorageService;
+
+    @Resource(name = "messageSourceAccessor")
+    private MessageSourceAccessor messageSourceAccessor;
 
     @PostMapping
     @AuthRequired
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public RestResponse<String> handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException, InvalidFileException {
+    public RestResponse<String> handleFileUpload(@RequestPart MultipartFile file) throws IOException, InvalidFileException {
+        if (file.getSize() > imageMaxSize) {
+            throw new InvalidFileException(messageSourceAccessor.getMessage(Message.IMAGE_SIZE_EXCEEDED));
+        }
+
         return RestResponse.success(imageStorageService.store(file));
     }
-
 
     @ExceptionHandler(IOException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -40,6 +50,6 @@ public class ImageUploadController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public RestResponse<?> handleInvalidFileException(InvalidFileException exception) {
-        return RestResponse.error("").build();
+        return RestResponse.error(exception.getMessage()).build();
     }
 }
